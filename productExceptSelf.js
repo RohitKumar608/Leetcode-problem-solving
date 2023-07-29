74a0b028bd36dd475393c61414d7b3d0d7caf196
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-07-29 12:51:14 +0530

    feat: optimize the code for productExceptSelf problem

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -5,13 +5,20 @@
  */
 var productExceptSelf = function (nums) {
   const result = []
+  const prefix = [1]
+  const postFix = []
+  postFix[nums.length] = 1
   for (let i = 0; i < nums.length; i++) {
-    result[i] = nums.reduce((prev, curr, currentIndex) => {
-      if (currentIndex === i) return prev
-      return prev * curr
-    }, 1)
+    prefix[i + 1] = prefix[prefix.length - 1] * nums[i]
+    postFix[nums.length - 1 - i] =
+      postFix[nums.length - i] * nums[nums.length - 1 - i]
+  }
+  for (let idx = 1; idx <= nums.length; idx++) {
+    result[idx - 1] =
+      prefix[idx - 1] * postFix[idx] === -0 ? 0 : prefix[idx - 1] * postFix[idx]
   }
   return result
 }
 
-// console.log(productExceptSelf([1, 2, 3, 4]))
+console.log(productExceptSelf([1, 2, 3, 4]))
+console.log(productExceptSelf([-1, 1, 0, -3, 3]))
