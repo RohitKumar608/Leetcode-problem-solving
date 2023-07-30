1414b83b65b32fc2046b7be5dbef247299bdf979
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-07-30 11:47:33 +0530

    feat: solved lengthOfLongestSubstring problem using the window sliding method

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -8,16 +8,15 @@
 var lengthOfLongestSubstring = function (s) {
   let str = ''
   let maxLength = 0
-  let left = 0
   let right = 0
   while (right < s.length) {
-    str = s.substring(left, right + 1)
-    maxLength = Math.max(maxLength, right + 1 - left)
-    right++
-    while (str.includes(s[right]) && left < right) {
-      str = s.substring(left, right + 1)
-      left++
+    str = str + s[right]
+    while (s[right + 1] && str.includes(s[right + 1])) {
+      maxLength = Math.max(maxLength, str.length)
+      str = str.slice(1)
     }
+    maxLength = Math.max(maxLength, str.length)
+    right++
   }
   return maxLength
 }
@@ -26,3 +25,5 @@ console.log(lengthOfLongestSubstring('abcabcbb'))
 console.log(lengthOfLongestSubstring('pwwkew'))
 console.log(lengthOfLongestSubstring('bbbbb'))
 console.log(lengthOfLongestSubstring('dvdf'))
+console.log(lengthOfLongestSubstring(' '))
+console.log(lengthOfLongestSubstring('au'))
