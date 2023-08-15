58e6c616e72fffe3f1aee99ef3b15d3062dfb1ac
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-08-15 16:47:12 +0530

    optimized Insert Delete GetRandom O(1) problem using the swap method

--- insert-delete-getrandom.js
+++ insert-delete-getrandom.js
@@ -1,6 +1,7 @@
 var RandomizedSet = function () {
   this.randomizedSet = {}
-  this.data = []
+  this.randomSetArray = []
+  this.length = 0
 }
 
 /**
@@ -8,11 +9,12 @@ var RandomizedSet = function () {
  * @return {boolean}
  */
 RandomizedSet.prototype.insert = function (val) {
-  if (this.randomizedSet[val]) {
+  if (this.randomizedSet[val] >= 0) {
     return false
   }
-  this.randomizedSet[val] = true
-  this.data.push(val)
+  this.randomizedSet[val] = this.length
+  this.randomSetArray.push(val)
+  this.length++
   return true
 }
 
@@ -21,9 +23,17 @@ RandomizedSet.prototype.insert = function (val) {
  * @return {boolean}
  */
 RandomizedSet.prototype.remove = function (val) {
-  if (this.randomizedSet[val]) {
-    this.randomizedSet[val] = false
-    this.data = this.data.filter((currVal) => currVal !== val)
+  if (this.randomizedSet[val] >= 0) {
+    const swapVal = this.randomSetArray.pop()
+    const swapToIdx = this.randomizedSet[val]
+    delete this.randomizedSet[val]
+    this.length--
+
+    if (swapToIdx === this.length) {
+      return true
+    }
+    this.randomizedSet[swapVal] = swapToIdx
+    this.randomSetArray[swapToIdx] = swapVal
     return true
   }
   return false
@@ -33,9 +43,8 @@ RandomizedSet.prototype.remove = function (val) {
  * @return {number}
  */
 RandomizedSet.prototype.getRandom = function () {
-  const randomNumber = Math.floor(Math.random() * this.data.length)
-  console.log(this.data)
-  return this.data[randomNumber]
+  const randomNumber = Math.floor(Math.random() * this.length)
+  return this.randomSetArray[randomNumber]
 }
 
 /**
@@ -46,12 +55,17 @@ RandomizedSet.prototype.getRandom = function () {
  * var param_3 = obj.getRandom()
  */
 var obj = new RandomizedSet()
-console.log(obj)
-console.log(obj.insert(1))
-console.log(obj.remove(2))
-console.log(obj.insert(2))
+// console.log(obj.insert(1))
+// console.log(obj.remove(2))
+// console.log(obj.insert(2))
 
-console.log(obj.getRandom())
-console.log(obj.remove(1))
-console.log(obj.insert(2))
+// console.log(obj.getRandom())
+// console.log(obj.remove(1))
+// console.log(obj.insert(2))
+// console.log(obj.getRandom())
+
+console.log(obj.insert(0))
+console.log(obj.remove(0))
+console.log(obj.insert(-1))
+console.log(obj.remove(0))
 console.log(obj.getRandom())
