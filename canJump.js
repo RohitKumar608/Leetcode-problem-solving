5d21480f5bd29a3ea0c1fe85bef9d7cf1df1d549
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-09-02 21:34:51 +0530

    solving canJum using the for loop

--- canJump.js
+++ canJump.js
@@ -6,26 +6,31 @@
 var canJump = function (nums) {
   if (nums.length == 0 || nums.length === 1) return true
   if (nums[0] === 0) return false
-  let firstVal = nums[0]
-  let sliceData = nums.slice(0, firstVal)
+  const sliceData = nums.slice(0, nums[0])
   let maxVal = Math.max(...sliceData)
-  let count = firstVal
-  if (count >= nums.length - 1) return true
-  if (nums[count] === 0 && nums[maxVal] === 0) return false
-  while (count < nums.length) {
-    if (count + maxVal >= nums.length - 1) return true
-    let sliceData = nums.slice(count, count + maxVal)
-    maxVal = Math.max(...sliceData)
-    count += maxVal
 
-    if (nums[count] === 0) return false
-    if (nums[count] === 0) return false
+  let index = sliceData.lastIndexOf(maxVal)
+  let count = 1
+  for (let idx = 0; idx < nums.length; idx++) {
+    if (count + index >= nums.length - 1) return true
+    let sliceData = nums.slice(count, count + maxVal)
+    const currentMax = Math.max(...sliceData)
+    index = sliceData.lastIndexOf(maxVal)
+    if (maxVal - currentMax > 0) {
+      count = currentMax
+    } else {
+      count += index + maxVal > 0 ? 1 : 0
+      maxVal = currentMax
+    }
   }
-  return true
+
+  return false
 }
 
-// console.log(canJump([2, 3, 1, 1, 4]))
-// console.log(canJump([3, 2, 1, 0, 4]))
-// console.log(canJump([1, 2, 0, 1]))
-// console.log(canJump([2, 5, 0, 0]))
+console.log(canJump([2, 3, 1, 1, 4]))
+console.log(canJump([3, 2, 1, 0, 4]))
+console.log(canJump([1, 2, 0, 1]))
+console.log(canJump([2, 5, 0, 0]))
 console.log(canJump([1, 1, 2, 2, 0, 1, 1]))
+
+console.log(canJump([4, 2, 0, 0, 1, 1, 4, 4, 4, 0, 4, 0]))
