5b68058ee7cc79767d52157c310d6300876046ea
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-09-29 21:39:00 +0530

    ref: First Unique Character in a String using

--- firstUniqChar.js
+++ firstUniqChar.js
@@ -3,7 +3,7 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function (s) {
+var firstUniqChar1 = function (s) {
   const mapSet = new Map()
   for (let i = 0; i < s.length; i++) {
     if (mapSet.has(s[i])) {
@@ -16,6 +16,24 @@ var firstUniqChar = function (s) {
   for (const [key, value] of mapSet) {
     if (value.val === 1) {
       return value.idx
+    }
+  }
+  return -1
+}
+
+var firstUniqChar = function (s) {
+  const mapSet = {}
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] in mapSet) {
+      mapSet[s[i]] = { val: 2, idx: i }
+    } else {
+      mapSet[s[i]] = { val: 1, idx: i }
+    }
+  }
+
+  for (const key in mapSet) {
+    if (mapSet[key].val === 1) {
+      return mapSet[key].idx
     }
   }
   return -1
