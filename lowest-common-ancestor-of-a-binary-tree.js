aa622864de43ddbb964394dbdf3a26c35f1134e0
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-10-14 23:09:28 +0530

    feat: solved lowest-common-ancestor-of-a-binary-tree leecode problem using recursion

--- lowest-common-ancestor-of-a-binary-tree.js
+++ lowest-common-ancestor-of-a-binary-tree.js
@@ -14,153 +14,51 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-  console.log(JSON.stringify(root))
-  console.log(JSON.stringify(p))
-  console.log(JSON.stringify(q))
-  if (root === null) return root
-  let result = []
-  if (root.left.val === p.val || root.right.val === p.val) {
-    result.push(root)
-  }
-  if (root.left.val === q.val || root.right.val === q.val) {
-    result.push(root)
-  }
-  const ancestors = []
-  const traveledTree = {}
-  const stack = [root]
-  while (stack.length) {
-    const node = stack.shift()
-    ancestors.push(node.val)
-    traveledTree[node.val] = node
-    // check for root node is ancestor
-    if (
-      (node?.left?.val === p.val && node?.right?.val === q.val) ||
-      (node?.left?.val === q.val && node?.right?.val === p.val)
-    ) {
-      result.push(node)
+  function traverse(root) {
+    if (root === null) return null
+    if (root.val === p.val || root.val === q.val) {
+      return root
     }
-    //check if the p node is the ancestor
-    if (
-      (node.val === p.val || ancestors.includes(p.val)) &&
-      (node?.left?.val === q.val || node?.right?.val === q.val)
-    ) {
-      ancestors.includes(p.val)
-        ? result.push(traveledTree[p.val])
-        : result.push(node)
+    if (root?.left === null && root.right === null) return null
+    let left = null
+    let right = null
+
+    if (root?.left) {
+      left = traverse(root.left)
     }
-    //check if the q node is the ancestor
-    if (
-      (node.val === q.val || ancestors.includes(q.val)) &&
-      (node?.left?.val === p.val || node?.right?.val === p.val)
-    ) {
-      ancestors.includes(q.val)
-        ? result.push(traveledTree[q.val])
-        : result.push(node)
+    if (root?.right) {
+      right = traverse(root.right)
     }
-    if (node?.left) stack.push(node.left)
-    if (node?.right) stack.push(node.right)
+    if (right && left) {
+      return root
+    }
+    return left === null ? right : left
   }
-  console.log(result)
-  const minVal = Math.min(...result.map((obj) => obj.val))
-  return result.find((obj) => obj.val === minVal)
+  const result = traverse(root, p, q)
+
+  return result
 }
-
-// console.log(
-//   lowestCommonAncestor(
-//     {
-//       val: 1,
-//       left: { val: 2, left: null, right: null },
-//       right: { val: 3, left: null, right: null },
-//     },
-//     { val: 2, left: null, right: null },
-//     { val: 3, left: null, right: null }
-//   )
-// )
-
-// console.log(
-//   lowestCommonAncestor(
-//     {
-//       val: 3,
-//       left: {
-//         val: 5,
-//         left: { val: 6, left: null, right: null },
-//         right: {
-//           val: 2,
-//           left: { val: 7, left: null, right: null },
-//           right: { val: 4, left: null, right: null },
-//         },
-//       },
-//       right: {
-//         val: 1,
-//         left: { val: 0, left: null, right: null },
-//         right: { val: 8, left: null, right: null },
-//       },
-//     },
-//     {
-//       val: 5,
-//       left: { val: 6, left: null, right: null },
-//       right: {
-//         val: 2,
-//         left: { val: 7, left: null, right: null },
-//         right: { val: 4, left: null, right: null },
-//       },
-//     },
-//     {
-//       val: 1,
-//       left: { val: 0, left: null, right: null },
-//       right: { val: 8, left: null, right: null },
-//     }
-//   )
-// )
-
-// console.log(
-//   lowestCommonAncestor(
-//     {
-//       val: 3,
-//       left: {
-//         val: 5,
-//         left: { val: 6, left: null, right: null },
-//         right: {
-//           val: 2,
-//           left: { val: 7, left: null, right: null },
-//           right: { val: 4, left: null, right: null },
-//         },
-//       },
-//       right: {
-//         val: 1,
-//         left: { val: 0, left: null, right: null },
-//         right: { val: 8, left: null, right: null },
-//       },
-//     },
-//     {
-//       val: 5,
-//       left: { val: 6, left: null, right: null },
-//       right: {
-//         val: 2,
-//         left: { val: 7, left: null, right: null },
-//         right: { val: 4, left: null, right: null },
-//       },
-//     },
-//     { val: 4, left: null, right: null }
-//   )
-// )
 
 console.log(
   lowestCommonAncestor(
     {
-      val: -1,
+      val: 3,
       left: {
-        val: 0,
-        left: {
-          val: -2,
-          left: { val: 8, left: null, right: null },
-          right: null,
+        val: 5,
+        left: { val: 6, left: null, right: null },
+        right: {
+          val: 2,
+          left: { val: 7, left: null, right: null },
+          right: { val: 4, left: null, right: null },
         },
-        right: { val: 4, left: null, right: null },
       },
-      right: { val: 3, left: null, right: null },
+      right: {
+        val: 1,
+        left: { val: 0, left: null, right: null },
+        right: { val: 8, left: null, right: null },
+      },
     },
-    { val: 3, left: null, right: null },
-    { val: 8, left: null, right: null }
+    { val: 6, left: null, right: null },
+    { val: 4, left: null, right: null }
   )
 )
