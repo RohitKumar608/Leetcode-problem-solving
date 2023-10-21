c68ef0d2142c1324a4449636179907557ecab1e5
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-10-21 06:36:42 +0530

    solved binary-tree-right-side-view leetcode problem

--- binary-tree-right-side-view.js
+++ binary-tree-right-side-view.js
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/binary-tree-right-side-view
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -15,29 +16,14 @@ var rightSideView = function (root) {
   console.log(JSON.stringify(root))
   const result = []
   const stack = [root]
-  const secondStack = []
-  let count = 0
   while (stack.length) {
     const length = stack.length
     for (let i = 0; i < length; i++) {
       const node = stack.shift()
-      if (node?.left) secondStack.push(node.left)
       if (node?.right) stack.push(node.right)
+      if (node?.left && !node?.right?.right) stack.push(node.left?.left)
       if (node?.val) result.push(node.val)
     }
-    count++
-  }
-  let dupCount = 1
-  while (secondStack.length) {
-    const length = secondStack.length
-    for (let i = 0; i < length; i++) {
-      const node = secondStack.shift()
-      if (node?.left) secondStack.push(node.left)
-      if (dupCount >= count) {
-        if (node?.val) result.push(node.val)
-      }
-    }
-    dupCount++
   }
 
   return result
@@ -46,15 +32,19 @@ var rightSideView = function (root) {
 console.log(
   rightSideView({
     val: 1,
-    left: { val: 2, left: { val: 4, left: null, right: null }, right: null },
-    right: { val: 3, left: null, right: null },
-  })
-)
-
-console.log(
-  rightSideView({
-    val: 1,
-    left: { val: 2, left: null, right: null },
-    right: null,
+    left: null,
+    right: {
+      val: 2,
+      left: null,
+      right: {
+        val: 5,
+        left: {
+          val: 4,
+          left: { val: 3, left: null, right: null },
+          right: null,
+        },
+        right: { val: 6, left: null, right: null },
+      },
+    },
   })
 )
