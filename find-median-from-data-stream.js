406a6c1579731eaf3cef253148003f20189ed168
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-11-01 22:31:08 +0530

    potimized find-median-from-data-stream leetcode problem using max and min heap

--- find-median-from-data-stream.js
+++ find-median-from-data-stream.js
@@ -41,6 +41,32 @@ MedianFinder.prototype.findMedian = function () {
  * obj.addNum(num)
  * var param_2 = obj.findMedian()
  */
+
+class MedianFinder {
+  //Initilize max and min heap
+  constructor() {
+    this.minHeap = new MinPriorityQueue() //supported by leetcode
+    this.maxHeap = new MaxPriorityQueue()
+  }
+
+  addNum(num) {
+    //add to min and pop the top for max to keep them in the order that we want
+    this.minHeap.enqueue(num)
+    this.maxHeap.enqueue(this.minHeap.dequeue().element)
+    //balance them
+    if (this.minHeap.size() < this.maxHeap.size()) {
+      this.minHeap.enqueue(this.maxHeap.dequeue().element)
+    }
+  }
+
+  findMedian() {
+    if (this.minHeap.size() > this.maxHeap.size())
+      return this.minHeap.front().element
+    else
+      return (this.minHeap.front().element + this.maxHeap.front().element) / 2
+  }
+}
+
 var instance = new MedianFinder()
 
 const data = [
