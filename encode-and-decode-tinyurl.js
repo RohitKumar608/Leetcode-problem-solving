b9e28fbde77758018ab57d341c86f165bbf993c9
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-11-12 22:52:22 +0530

    solved encode-and-decode leetcode problem

---
+++ encode-and-decode-tinyurl.js
@@ -0,0 +1,18 @@
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+let map = new Map()
+let url = 'http://tinyurl.com/'
+var encode = function (longUrl) {
+  let key = Date.now().toString()
+  map.set(key, longUrl)
+  return url + key
+}
+
+var decode = function (shortUrl) {
+  let key = shortUrl.split(url)
+  return map.get(key[1])
+}
