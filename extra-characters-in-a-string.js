dddff148e31c6d4faaf3ae099920d8ea0d6c4510
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-11-21 22:25:47 +0530

    solved extra-characters-in-a-string leetcode problem

--- extra-characters-in-a-string.js
+++ extra-characters-in-a-string.js
@@ -4,14 +4,28 @@
  * @return {number}
  */
 var minExtraChar = function (s, dictionary) {
-  dictionary.sort((a, b) => b.length - a.length)
-  dictionary.forEach((val) => {
-    s = s.split(val).join('')
-  })
-  return s.length
+  const words = new Set(dictionary)
+  const length = s.length
+  const dp = { [s.length]: 0 }
+  function dfs(i) {
+    if (i in dp) {
+      return dp[i]
+    }
+    let res = 1 + dfs(i + 1)
+
+    for (let j = i; j < length; j++) {
+      if (words.has(s.slice(i, j + 1))) {
+        res = Math.min(res, dfs(j + 1))
+      }
+    }
+    dp[i] = res
+
+    return res
+  }
+  return dfs(0)
 }
 
-// console.log(minExtraChar('leetscode', ['leet', 'code', 'leetcode']))
+console.log(minExtraChar('leetscode', ['leet', 'code', 'leetcode']))
 console.log(
   minExtraChar('dwmodizxvvbosxxw', [
     'ox',
