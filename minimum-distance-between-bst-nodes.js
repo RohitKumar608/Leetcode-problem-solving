c386f26573c6bfae0728d1b07e9c7d709fa640b4
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-01-16 23:46:34 +0530

    solved 783 leetcode minimum-distance-between-bst-nodes problem

---
+++ minimum-distance-between-bst-nodes.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function (root) {
+  let totalmin = Infinity
+  let prev = Infinity
+  function traverse(node) {
+    if (!node) return
+    traverse(node.left)
+    totalmin = Math.min(totalmin, Math.abs(node.val - prev))
+    prev = node.val
+    traverse(node.right)
+  }
+  traverse(root)
+  return totalmin
+}
