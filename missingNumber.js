c43b1d078585c0030a4f6961e0ee33b433f39672
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-01-17 23:49:16 +0530

    solved missing-number leetcode problem

---
+++ missingNumber.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/missing-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var missingNumber = function (nums) {
+  let sum = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+  }
+  return (nums.length * (nums.length + 1)) / 2 - sum
+}
