a46b5e08c905e93b70c392d8249b6c64c5851b6f
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-02-20 10:43:53 +0530

    solved 326 power-of-three leetcode problem

---
+++ 326_power-of-three.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/power-of-three
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function (n) {
+  while (n >= 1) {
+    if (n === 1) {
+      return true
+    }
+    n = n / 3
+  }
+  return false
+}
