325dc841fef098098b8a24a225f38b9b5f072d89
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-04-01 21:05:37 +0530

    solved 796 rotate-string leetcode problem

---
+++ 796_rotate-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) {
+    return false
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.substring(i) + s.substring(0, i) === goal) return true
+  }
+
+  return false
+}
+
+console.log(rotateString('abcde', 'cdeab'))
