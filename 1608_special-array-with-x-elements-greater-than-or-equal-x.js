8b5afb95dbdafa15d1381e01735acf1b661bf033
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-05-27 22:32:05 +0530

    optimized the code 1608 special-array-with-x-elements-greater-than-or-equal-x

--- 1608_special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608_special-array-with-x-elements-greater-than-or-equal-x.js
@@ -4,7 +4,6 @@
  */
 var specialArray = function (nums) {
   nums.sort((a, b) => a - b)
-  const len = nums.length
   for (let i = 1; i <= 1000; i++) {
     const greaterEqualValLen = nums.filter((val) => val >= i).length
     if (greaterEqualValLen === i) {
@@ -14,4 +13,20 @@ var specialArray = function (nums) {
   return -1
 }
 
-console.log(specialArray([0, 4, 3, 0, 4]))
+var specialArray = function (nums) {
+  const prefix = new Array(nums.length + 1).fill(0)
+  for (const num of nums) {
+    prefix[Math.min(num, nums.length)]++
+  }
+  let totalRight = 0
+  for (let i = nums.length; i >= 1; i--) {
+    totalRight += prefix[i]
+    if (totalRight === i) {
+      return i
+    }
+  }
+  return -1
+}
+
+// console.log(specialArray([0, 4, 3, 0, 4]))
+console.log(specialArray([3, 5]))
