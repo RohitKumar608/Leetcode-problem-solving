fb067e3aea9435fe6696598abdc1f11ed9b65115
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-08-10 12:17:08 +0530

    feat: refactor the code to maitain the readability 840 magic-squares-in-grid

--- 840_magic-squares-in-grid.js
+++ 840_magic-squares-in-grid.js
@@ -2,64 +2,45 @@
  * @param {number[][]} grid
  * @return {number}
  */
+
+function isMagicSquare(matrix, sum = 15) {
+  const visited = {}
+  const sumOfRowColDia = { [sum]: 0 }
+  for (let i = 0; i < 3; i++) {
+    let rowSum = 0
+    let colSum = 0
+    for (let j = 0; j < 3; j++) {
+      if (visited[matrix[i][j]] || matrix[i][j] < 1 || matrix[i][j] > 9) {
+        return false
+      }
+
+      visited[matrix[i][j]] = true
+      rowSum += matrix[i][j]
+      colSum += matrix[j][i]
+    }
+    sumOfRowColDia[rowSum]++
+    sumOfRowColDia[colSum]++
+  }
+  let leftDiagonalSum = 0
+  let rightDiagonalSum = 0
+  for (let i = 0; i < 3; i++) {
+    leftDiagonalSum += matrix[i][i]
+    rightDiagonalSum += matrix[2 - i][i]
+  }
+  sumOfRowColDia[leftDiagonalSum]++
+  sumOfRowColDia[rightDiagonalSum]++
+  return sumOfRowColDia[sum] === 8
+}
+
 var numMagicSquaresInside = function (grid) {
   const row = grid.length
   const col = grid[0].length
-
-  if (row < 3 || col < 3) return 0
-
-  const isValid = (matrix) => {
-    if (matrix[1][1] !== 5) {
-      return false
-    }
-
-    const visited = {}
-    let expectedSum = 0
-    for (let i = 0; i < 3; i++) {
-      let sum = 0
-      for (let j = 0; j < 3; j++) {
-        if (matrix[i][j] >= 1 && matrix[i][j] <= 9 && !visited[matrix[i][j]]) {
-          visited[matrix[i][j]] = true
-        } else {
-          return false
-        }
-        sum += matrix[i][j]
-      }
-      if (i === 0) expectedSum = sum
-
-      if (sum !== expectedSum) {
-        return false
-      }
-    }
-
-    for (let j = 0; j < 3; j++) {
-      let sum = 0
-      for (let i = 0; i < 3; i++) {
-        sum += matrix[i][j]
-      }
-      if (sum !== expectedSum) {
-        return false
-      }
-    }
-
-    let sumA = 0,
-      sumB = 0
-    for (let i = 0; i < 3; i++) {
-      sumA += matrix[i][i]
-      sumB += matrix[i][2 - i]
-    }
-    if (sumA !== expectedSum || sumB !== expectedSum) {
-      return false
-    }
-
-    return true
-  }
-
   let answer = 0
+  if (row < 3 || col < 3) return 0
   for (let i = 0; i <= row - 3; i++) {
     const m = grid.slice(i, i + 3)
     for (let j = 0; j <= col - 3; j++) {
-      if (isValid(m.map((o) => o.slice(j, j + 3)))) {
+      if (isMagicSquare(m.map((o) => o.slice(j, j + 3)))) {
         answer++
       }
     }
