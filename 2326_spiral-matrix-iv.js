4fd4c9f5dca6a666c77f1f2411b70313b6734fcf
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-09-09 09:55:09 +0530

    Solved daily leetcode challenge 2326 spiral-matrix-iv

--- 2326_spiral-matrix-iv.js
+++ 2326_spiral-matrix-iv.js
@@ -115,43 +115,6 @@ var spiralMatrix = function (m, n, head) {
   return a
 }
 
-// console.log(
-//   spiralMatrix(3, 5, {
-//     val: 3,
-//     next: {
-//       val: 0,
-//       next: {
-//         val: 2,
-//         next: {
-//           val: 6,
-//           next: {
-//             val: 8,
-//             next: {
-//               val: 1,
-//               next: {
-//                 val: 7,
-//                 next: {
-//                   val: 9,
-//                   next: {
-//                     val: 4,
-//                     next: {
-//                       val: 2,
-//                       next: {
-//                         val: 5,
-//                         next: { val: 5, next: { val: 0, next: null } },
-//                       },
-//                     },
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   })
-// )
-
 console.log(
   spiralMatrix(10, 1, {
     val: 8,
