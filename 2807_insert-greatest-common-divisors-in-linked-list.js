2d2fe0be69882ce2d5fcd7b11b279425395770c3
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-09-10 11:02:58 +0530

    Solved daily leetcode challenge 2807 insert-greatest-common-divisors-in-linked-list

--- 2807_insert-greatest-common-divisors-in-linked-list.js
+++ 2807_insert-greatest-common-divisors-in-linked-list.js
@@ -34,10 +34,3 @@ function ListNode(val, next) {
   this.val = val === undefined ? 0 : val
   this.next = next === undefined ? null : next
 }
-
-console.log(
-  insertGreatestCommonDivisors({
-    val: 18,
-    next: { val: 6, next: { val: 10, next: { val: 3, next: null } } },
-  })
-)
