75af2b8474bc0583c525c9b9e678208f84c93ac5
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-09-14 20:35:11 +0530

    Solved daily leetcode challenge 2419 longest-subarray-with-maximum-bitwise-and

---
+++ 2419_longest-subarray-with-maximum-bitwise-and.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function (nums) {
+  let maxVal = Math.max(...nums)
+  let result = 0
+  let tempMaxCount = 1
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1] && nums[i] === maxVal) {
+      tempMaxCount++
+    } else {
+      result = Math.max(result, tempMaxCount)
+      tempMaxCount = 1
+    }
+  }
+  return result
+}
+
+console.log(longestSubarray([1, 2, 3, 3, 2, 2]))
