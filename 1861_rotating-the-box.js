2e90e18071289c6f58a4fbcd15a017bc4b257793
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-11-23 10:57:34 +0530

    Solved daily leetcode challenge 1861 rotating-the-box

---
+++ 1861_rotating-the-box.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[][]} box
+ * @return {character[][]}
+ */
+var rotateTheBox = function (box) {
+  let m = box.length
+  let n = box[0].length
+  let rotatedBox = Array.from({ length: n }, () => Array(m).fill('.'))
+
+  for (let i = 0; i < m; i++) {
+    let currentIndex = n - 1
+
+    for (let j = n - 1; j >= 0; j--) {
+      let val = box[i][j]
+
+      if (val === '.') continue
+      if (val === '*') currentIndex = j
+
+      rotatedBox[currentIndex][m - 1 - i] = val
+      currentIndex--
+    }
+  }
+
+  return rotatedBox
+}
+
+console.log(
+  rotateTheBox([
+    ['#', '#', '*', '.', '*', '.'],
+    ['#', '#', '#', '*', '.', '.'],
+    ['#', '#', '#', '.', '#', '.'],
+  ])
+)
