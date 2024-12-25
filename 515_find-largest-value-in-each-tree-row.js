d7b085233d53a778d866dd31d5ac217c38e4e23a
rohitkumarm <rohitkumarrk13568@gmail.com>
2024-12-25 10:56:23 +0530

    Solved daily leetcode challenge 515 find-largest-value-in-each-tree-row

---
+++ 515_find-largest-value-in-each-tree-row.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function (root) {
+  const largestValuesPerLevel = []
+
+  function dfs(node, level) {
+    if (node === null) {
+      return
+    }
+
+    if (largestValuesPerLevel[level] === undefined) {
+      largestValuesPerLevel[level] = node.val
+    } else {
+      largestValuesPerLevel[level] = Math.max(
+        largestValuesPerLevel[level],
+        node.val
+      )
+    }
+
+    dfs(node.left, level + 1)
+    dfs(node.right, level + 1)
+  }
+
+  dfs(root, 0)
+  return largestValuesPerLevel
+}
