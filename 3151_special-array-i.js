2bb297026efdba5fbabadabdc636652abf780424
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-02-01 18:24:00 -1200

    Solved daily leetcode challenge 3151 special-array-i

---
+++ 3151_special-array-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function (nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] % 2 !== 0 && nums[i + 1] % 2 !== 0) {
+      return false
+    }
+    if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0) {
+      return false
+    }
+  }
+  return true
+}
