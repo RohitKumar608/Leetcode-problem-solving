8a3fe8f10a618adfb95637c5a0e732bf5901fa9d
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-04-29 10:06:48 +0530

    Solved daily leetcode challenge 2962 count-subarrays-where-max-element-appears-at-least-k-times

--- 2962_count-subarrays-where-max-element-appears-at-least-k-times.js
+++ 2962_count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -20,25 +20,27 @@ var countSubarrays = function (nums, k) {
   return result
 }
 
+var countSubarrays = function (nums, k) {
+  let maxNumber = 0
+  let left = 0
+  let maxNumberCount = 0
+  let result = 0
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] > maxNumber) {
+      maxNumber = nums[right]
+      maxNumberCount = 0
+      result = 0
+      left = 0
+    }
+    if (maxNumber === nums[right]) maxNumberCount++
+    while (maxNumberCount >= k) {
+      result += nums.length - right
+      if (nums[left] === maxNumber) maxNumberCount--
+      left++
+    }
+  }
+  return result
+}
+
 // console.log(countSubarrays([1, 3, 2, 3, 3], 2))
 // console.log(countSubarrays([1, 4, 2, 1], 3))
-console.log(
-  countSubarrays(
-    [
-      61, 23, 38, 23, 56, 40, 82, 56, 82, 82, 82, 70, 8, 69, 8, 7, 19, 14, 58,
-      42, 82, 10, 82, 78, 15, 82,
-    ],
-    2
-  )
-)
-
-console.log(
-  countSubarrays(
-    [
-      37, 20, 38, 66, 34, 38, 9, 41, 1, 14, 25, 63, 8, 12, 66, 66, 60, 12, 35,
-      27, 16, 38, 12, 66, 38, 36, 59, 54, 66, 54, 66, 48, 59, 66, 34, 11, 50,
-      66, 42, 51, 53, 66, 31, 24, 66, 44, 66, 1, 66, 66, 29, 54,
-    ],
-    5
-  )
-)
