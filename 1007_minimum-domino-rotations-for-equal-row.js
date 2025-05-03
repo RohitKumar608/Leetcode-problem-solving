581fcbf0a1a9418f4f0722896bf4b1675d746808
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-05-03 13:22:45 +0530

    Optimized the code for daily leetcode challenge 1007 minimum-domino-rotations-for-equal-row

--- 1007_minimum-domino-rotations-for-equal-row.js
+++ 1007_minimum-domino-rotations-for-equal-row.js
@@ -4,42 +4,56 @@
  * @return {number}
  */
 var minDominoRotations = function (tops, bottoms) {
-  const topSliceFreq = new Map()
-  const bottomsSliceFreq = new Map()
+  const sliceFrequency = new Map()
   let maxSliceCount = 0
-  let swapSlice = ''
-  let swapFrom = ''
-  let result = 0
+  let maxSlices = ''
+  let rotateTop = 0
+  let rotateBottom = 0
   for (let i = 0; i < tops.length; i++) {
-    topSliceFreq.set(tops[i], (topSliceFreq.get(tops[i]) || 0) + 1)
-    bottomsSliceFreq.set(
-      bottoms[i],
-      (bottomsSliceFreq.get(bottoms[i]) || 0) + 1
-    )
-  }
-  for (const [key, topSliceCount] of topSliceFreq) {
-    const bottomSliceCount = bottomsSliceFreq.get(key)
-    const totalSliceCount = topSliceCount + bottomSliceCount
-    if (totalSliceCount > maxSliceCount) {
-      maxSliceCount = totalSliceCount
-      swapSlice = +key
-      swapFrom = topSliceCount > bottomSliceCount ? 'bottom' : 'top'
+    sliceFrequency.set(tops[i], (sliceFrequency.get(tops[i]) || 0) + 1)
+    sliceFrequency.set(bottoms[i], (sliceFrequency.get(bottoms[i]) || 0) + 1)
+    if (sliceFrequency.get(tops[i]) > maxSliceCount) {
+      maxSlices = tops[i]
+      maxSliceCount = sliceFrequency.get(tops[i])
+    }
+    if (sliceFrequency.get(bottoms[i]) > maxSliceCount) {
+      maxSlices = bottoms[i]
+      maxSliceCount = sliceFrequency.get(bottoms[i])
     }
   }
   if (tops.length > maxSliceCount) {
     return -1
   }
   for (let i = 0; i < tops.length; i++) {
-    if (tops[i] !== swapSlice && bottoms[i] !== swapSlice) {
+    if (tops[i] !== maxSlices && bottoms[i] !== maxSlices) {
       return -1
     }
     if (tops[i] === bottoms[i]) continue
-    if (swapFrom == 'bottom' && bottoms[i] == swapSlice) {
-      result++
-    }
-    if (swapFrom == 'top' && tops[i] == swapSlice) result++
+    if (bottoms[i] == maxSlices) rotateTop++
+    if (tops[i] == maxSlices) rotateBottom++
   }
-  return result
+  return Math.min(rotateBottom, rotateTop)
+}
+
+function minDominoRotations(tops, bottoms) {
+  let res = getRotation(tops, bottoms, tops[0])
+  if (bottoms[0] !== tops[0]) {
+    res = Math.min(res, getRotation(tops, bottoms, bottoms[0]))
+  }
+  return res === Infinity ? -1 : res
+}
+
+function getRotation(tops, bottoms, target) {
+  let rotateTop = 0,
+    rotateBottom = 0
+  for (let i = 0; i < tops.length; i++) {
+    if (tops[i] !== target && bottoms[i] !== target) {
+      return Infinity
+    }
+    if (tops[i] !== target) rotateTop++
+    if (bottoms[i] !== target) rotateBottom++
+  }
+  return Math.min(rotateTop, rotateBottom)
 }
 
 console.log(minDominoRotations([2, 1, 2, 4, 2, 2], [5, 2, 6, 2, 3, 2]))
