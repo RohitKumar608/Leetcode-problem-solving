4ce2be4a3287687b230b4964dd980c989bc920c4
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-06-08 09:43:38 +0530

    Solved daily leetcode challenge 386 lexicographical-numbers

--- 386_lexicographical-numbers.js
+++ 386_lexicographical-numbers.js
@@ -52,5 +52,22 @@ var lexicalOrder2 = function (n) {
   return arr
 }
 
+var lexicalOrder = function (n) {
+  const result = []
+  function dfs(curr) {
+    if (curr > n) return
+    result.push(curr)
+    for (let i = 0; i <= 9; i++) {
+      const next = curr * 10 + i
+      if (next > n) return
+      dfs(next)
+    }
+  }
+  for (let i = 1; i <= 9; i++) {
+    dfs(i)
+  }
+  return result
+}
+
 console.log(lexicalOrder(1003))
 console.log(lexicalOrder2(1003))
