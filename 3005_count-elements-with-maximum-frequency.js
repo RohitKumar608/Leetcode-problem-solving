be42e066be432ec1d9eb30555634906b07375bc7
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-09-22 08:49:21 +0530

    Solved daily leetcode challenge 3005 count-elements-with-maximum-frequency

--- 3005_count-elements-with-maximum-frequency.js
+++ 3005_count-elements-with-maximum-frequency.js
@@ -18,4 +18,27 @@ var maxFrequencyElements = function (nums) {
   return result
 }
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var maxFrequencyElements = function (nums) {
+  let map = new Map()
+  let maxFrequency = 0
+  let ans = 0
+  for (let num of nums) {
+    const freq = (map.get(num) || 0) + 1
+    map.set(num, freq)
+    maxFrequency = Math.max(maxFrequency, freq)
+  }
+
+  for (let [k, v] of map.entries()) {
+    if (v === maxFrequency) {
+      ans += v
+    }
+  }
+  return ans
+}
+
 console.log(maxFrequencyElements([1, 2, 2, 3, 1, 4]))
