3d99c8ee85398a1e34fd028e3d18122cc001c961
rohitkumarm <rohitkumarrk13568@gmail.com>
2025-10-09 21:34:11 +0530

    Solved leetcode challenge 1409 queries-on-a-permutation-with-key

---
+++ 1409_queries-on-a-permutation-with-key.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} queries
+ * @param {number} m
+ * @return {number[]}
+ */
+var processQueries = function (queries, m) {
+  const values = []
+  const result = []
+  for (let i = 1; i <= m; i++) {
+    values[i - 1] = i
+  }
+  for (let i = 0; i < queries.length; i++) {
+    const idx = values.indexOf(queries[i])
+    result[i] = idx
+    values.splice(idx, 1)
+    values.unshift(queries[i])
+  }
+  return result
+}
